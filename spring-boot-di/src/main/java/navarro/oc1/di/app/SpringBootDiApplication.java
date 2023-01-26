ac26e20ab35162b2f35aba4068525f1f44664a28
oc1navarro <oc1.navarro@gmail.com>
2023-01-26 18:52:06 +0100

    primer commit

---
+++ spring-boot-di/src/main/java/navarro/oc1/di/app/SpringBootDiApplication.java
@@ -0,0 +1,13 @@
+package navarro.oc1.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDiApplication.class, args);
+	}
+
+}
---
+++ spring-boot-di/src/test/java/navarro/oc1/di/app/SpringBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package navarro.oc1.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
