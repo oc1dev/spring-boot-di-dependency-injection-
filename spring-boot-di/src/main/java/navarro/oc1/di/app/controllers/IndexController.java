6216c4a21d8a456b3f20e550beeda171a75185fd
oc1navarro <oc1.navarro@gmail.com>
2023-01-27 09:25:31 +0100

    Inyectando mediante atributo, metodo setter y constructor

--- spring-boot-di/src/main/java/navarro/oc1/di/app/controllers/IndexController.java
+++ spring-boot-di/src/main/java/navarro/oc1/di/app/controllers/IndexController.java
@@ -10,12 +10,28 @@ import navarro.oc1.di.app.models.services.IServicio;
 @Controller
 public class IndexController {
 	
-	@Autowired
+	@Autowired //asi inyectariamos en el atributo
 	private IServicio servicio;
 	
+	
+	/*@Autowired //inyectando en el constructor
+	public IndexController(IServicio servicio) {
+		this.servicio = servicio;
+	}*/
+
+
+
 	@GetMapping({"/","","/index"})
 	public String index (Model model) {
 		model.addAttribute("objeto", servicio.operacion());
 		return "index";
 	}
+
+	
+	//inyectando en metodo setter
+	/*@Autowired
+	public void setServicio(IServicio servicio) {
+		this.servicio = servicio;
+	}*/
+	
 }
